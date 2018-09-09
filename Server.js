4178d2f0b45c8c1ec6a59fa36684e539926051b6
RyuSong <hopeness@daum.net>
2018-09-09 17:51:36 +0900

    Change Server.js
    change server.js

--- Server.js
+++ Server.js
@@ -1,19 +1,16 @@
-const http = require('http');
-http.createServer((request, response) => {
-  return request
-    .on('error', (err) => { // 요청에 에러가 있으면
-      console.error(err);
-    })
-    .on('data', (data) => { // 요청에 데이터가 있으면
-      console.log(data);
-    })
-    .on('end', () => { // 요청의 데이터가 모두 받아졌으면
-      response.on('error', (err) => { // 응답에 에러가 있으면
-        console.error(err);
-      });
-      response.statusCode = 200; // 성공 상태 코드
-       response.setHeader('Content-Type', 'text/plain'); // header 설정
-      response.write('hi\n'); // body에 정보 탑재
-      response.end('the end!'); // 정보 탑재 후 브라우저로 전송
-    });
-}).listen(3000);
+// 1. 서버 사용을 위해서 http 모듈을 http 변수에 담는다. (모듈과 변수의 이름은 달라도 된다.)
+var http = require('http');
+
+// 2. http 모듈로 서버를 생성한다.
+//    아래와 같이 작성하면 서버를 생성한 후, 사용자로 부터 http 요청이 들어오면 function 블럭내부의 코드를 실행해서 응답한다.
+var server = http.createServer(function(request,response){
+
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('Hello node.js!!');
+
+});
+
+// 3. listen 함수로 8080 포트를 가진 서버를 실행한다. 서버가 실행된 것을 콘솔창에서 확인하기 위해 'Server is running...' 로그를 출력한다
+server.listen(3000, function(){ 
+    console.log('Server is running...');
+});
